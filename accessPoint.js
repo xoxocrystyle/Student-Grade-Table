5756bd17f91c0a86a23669fdec8024dfb8aad93b
Crystal Shir Navarro <crystal.shir@gmail.com>
2018-02-01 17:48:04 -0800

    Moved my SGT from LFZ server, to my own node server

---
+++ accessPoint.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const path = require('path');
+const mysql= require('mysql');
+const mysqlCredentials = require('./mysqlCredentials.js');
+const ws = express();
+
+//creating an access point to db
+const db = mysql.createConnection(mysqlCredentials);
+
+//static files
+ws.use( express.static(path.join(__dirname, 'html')));
+
+//access to the user
+ws.get('/users', function(req, res){
+    db.connect(function(){
+        db.query('SELECT * FROM students', function(error, rows, fields){
+            //console.log(rows);
+            const output = {
+                success: true,
+                data: rows
+            }
+            const json_output = JSON.stringify(output);
+            //res.send closes connection
+            res.send(json_output);
+        })
+    })
+});
+
+
+// ws.post('/users:id', function(){
+
+// })
+// ws.delete('/users', function(){
+
+// })
+// ws.put('/users', function(){
+
+// })
+// ws.get('/items', function(){
+
+// })
+// ws.post('/items', function(){
+
+// })
+
+ws.listen(3000, function(){
+    console.log("Listening on port:3000");
+})
+
---
+++ mysqlcredentials.js
@@ -0,0 +1,9 @@
+const credentials = {
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'c12.17',
+    port: 8889
+}
+
+module.exports = credentials; 
